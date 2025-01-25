39c0afce50e6ca95fd8add6076ec8a10c62d9b7c
Michaek Gonyk <maximqwerbich@gmail.com>
2025-01-25 11:22:46 +0200

    Add files via upload

---
+++ dictionary/dictionary.py
@@ -0,0 +1,123 @@
+d = dict()
+d1 = {}
+
+chars = {
+    'a': 1,
+    'b': 2,
+}
+# .get()
+
+# c = chars.get('v', 'nema takogo')
+# print(c)
+
+
+chars['c'] = 3
+# print(chars)
+
+
+# .pop()
+# p = chars.pop('b')
+# print(chars)
+# print(p)
+
+
+
+# .clear()
+# chars.clear()
+
+# .update()
+
+chars = {'a': 1, 'b': 2,}
+
+chars.update({'d': 4})
+
+# print(chars)
+
+# .copy()
+# chp = chars.copy()
+# print( chp == chars)
+
+# Створити словник «місяці».
+# В якості ключа виступає порядковий номер місяця, в якості значення – його назва.
+
+monthes = {
+    1 : 'January',
+    2 : 'February',
+    3 : 'MArch',
+    4 : 'April'
+}
+# print(monthes[2])
+
+
+#курси валют
+
+money = {
+    'USD': 42.15,
+    'EUR': 45.01,
+    'PLN': 12.34
+}
+
+
+fruits = {
+    'watermelon': 222,
+    'banana': 124,
+    'grapes': 444
+}
+
+# print(fruits['banana'])
+
+# Створіть словник з назвами міст та їх кількістю населення. Виведіть на екран назву міста з найбільшою кількістю населення.
+
+cities = {
+    'Kyiv': 1500000,
+    'Mykolaiv': 500000,
+    'Ternopil': 250000,
+}
+
+# print(max(cities, key=cities.get))
+
+# Напишіть програму, яка дозволяє користувачам реєструватися на сайті та зберігає їх дані у словнику. 
+# Ключ — ім'я користувача, а значення — пароль.
+
+# usr = input('Enter usr name: ')
+# pswd = input('Enter password')
+
+user = {}
+
+# user[usr] = pswd
+
+# print(user)
+
+# Перетворення рядка у словник. Напишіть програму, яка перетворює рядок у словник, де ключ — перша буква слова, 
+# а значення — саме слово. Рядок вводиться користувачем з клавіатури.
+
+# txt = input('enter some text: ')
+
+# words = txt.split()
+
+# wd = {w[0]: w for w in words}
+
+# print(wd)
+
+
+# Гра "Хто є хто". У цій грі користувач повинен відгадати тварину, яку ми загадали.
+#  Ми надаємо користувачеві опис цієї тварини, включаючи її колір, розмір, дії і т.д. 
+# Користувач повинен ввести свій варіант тварини, а програма повинна повідомити користувачеві, чи він відгадав правильно, чи ні.
+
+animals = {
+    'cat': 'ця тварина мала, пухнаста, мявкає',
+    'dog': 'ця тварина трохи більша, пухнаста гавкає'
+}
+
+import random
+
+ani, descr = random.choice(list(animals.items()))
+
+print(descr)
+
+inp = input('Enter zdohadku: ')
+
+if ani == inp:
+    print('Ви вгадали')
+else:
+    print('ви не вгадали')
